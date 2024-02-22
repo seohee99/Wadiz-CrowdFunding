360b06de39359ef625bf146cb14e59e67ee761e8
seohee99 <imshee99@gmail.com>
2024-02-22 18:55:08 +0900

    [Feat] 라우터 경로 설정

--- back/app.js
+++ back/app.js
@@ -1,19 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var campaignRouter = require('./routes/campaign');
-var commentRouter = require('./routes/comment');
 var {connectDB, closeDB} = require('./data/dbConnect')
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors()); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,9 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // db connect
 connectDB();
-
 app.use('/api/', campaignRouter);
-app.use('/api/', commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
